1bc57167054a1609a5da873667453df5f73a6922
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2026-04-28 02:23:45 +0600

    init

--- app/app_entrypoint.prod.sh
+++ app/app_entrypoint.prod.sh
@@ -2,6 +2,8 @@
 
 python manage.py migrate
 
+python manage.py compilemessages
+
 python manage.py collectstatic --noinput
 
 gunicorn config.wsgi --workers 4 --bind 0.0.0.0:8000 --timeout 100 --log-level info
