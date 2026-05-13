a3b2a8786c6e8228471fae48e5795dcfff0bda30
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2026-05-13 14:04:11 +0600

    init

--- nginx/nginx_entrypoint.sh
+++ nginx/nginx_entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$get_certs_lower" = "true" ]; then
     folder_path="/etc/letsencrypt/live/$DOMAIN"
 
     if [ -d "$folder_path" ]; then
-        certbot -n --nginx \
+        certbot -n --nginx --expand \
           -d "$DOMAIN" \
           -d "www.$DOMAIN" \
           -d "$NODE_DOMAIN"
@@ -15,7 +15,7 @@ if [ "$get_certs_lower" = "true" ]; then
         nginx -s stop
         sleep 2
     else
-        certbot --nginx \
+        certbot --nginx --expand \
           --email "$CERTBOT_EMAIL" \
           --agree-tos \
           --no-eff-email \
